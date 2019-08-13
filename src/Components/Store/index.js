19cc4a42f1aec062c76799230d631c3457bb7e4a
vincentv5 <ebukavincent10@gmail.com>
2019-08-13 03:36:02 -0700

    redux

--- src/Components/Store/index.js
+++ src/Components/Store/index.js
@@ -2,30 +2,30 @@ import {compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './Reducer';
 
-// let store=null;
+let store=null;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, preloadedState, composeEnhancers(
-  applyMiddleware(thunk)
-));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, preloadedState, composeEnhancers(
+//   applyMiddleware(thunk)
+// ));
 
-// if(process.env.NODE_ENV === 'production') {
-//    store =createStore(
-// 		reducer,
-// 		compose(
-// 			applyMiddleware(thunk),	
-// 		)
-// 	);
-// } else {
-//     store =createStore(
-// 		reducer,
-// 		compose(
-// 			applyMiddleware(thunk),
-// 			window.devToolsExtension ? window.devToolsExtension() :
-// 			f=>f
-// 		)
-// 	);
-// }
+if(process.env.NODE_ENV === 'production') {
+   store =createStore(
+		reducer,
+		compose(
+			applyMiddleware(thunk),	
+		)
+	);
+} else {
+    store =createStore(
+		reducer,
+		compose(
+			applyMiddleware(thunk),
+			window.devToolsExtension ? window.devToolsExtension() :
+			f=>f
+		)
+	);
+}
 
 
 
