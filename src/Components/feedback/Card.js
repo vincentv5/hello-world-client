1a026de5f8a9254174ce85af8ac09cb2493f9a2c
vincentv5 <ebukavincent10@gmail.com>
2019-08-13 03:47:57 -0700

    redux

--- src/Components/feedback/Card.js
+++ src/Components/feedback/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {withRouter } from 'react-router-dom';
 const Card=(props)=>{
-	const card =props.feedbacks.map((val,i)=> (
+	const card =props.feedbacks?props.feedbacks.map((val,i)=> (
 		<div  
 		key={val._id} 
 		className = "rounded  pa1 center  mb3 pointer" 
@@ -9,7 +9,7 @@ const Card=(props)=>{
 			<p className='tc'>{val.message}</p>
 
 		</div> 
-))
+)):'';
 
 	return (
 			<React.Fragment>
