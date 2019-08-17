3e9afe5bb3d9b275f9e8fd186d7db850f7ff8596
vincentv5 <ebukavincent10@gmail.com>
2019-08-17 00:09:08 -0700

    reducer change

--- src/Components/Store/index.js
+++ src/Components/Store/index.js
@@ -1,31 +1,16 @@
-import {compose, createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './Reducer';
 
-// let store=null;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer,composeEnhancers(
-  applyMiddleware(thunk)
-));
 
-// if(process.env.NODE_ENV === 'production') {
-//    store =createStore(
-// 		reducer,
-// 		compose(
-// 			applyMiddleware(thunk),	
-// 		)
-// 	);
-// } else {
-//     store =createStore(
-// 		reducer,
-// 		compose(
-// 			applyMiddleware(thunk),
-// 			window.devToolsExtension ? window.devToolsExtension() :
-// 			f=>f
-// 		)
-// 	);
-// }
+
+const store = createStore(
+	reducer,
+  	applyMiddleware(thunk)
+);
+
+
 
 
 
